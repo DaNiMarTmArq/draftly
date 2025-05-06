645e5fa3831ab65dcdfe6b250cee9e45cf19f31c
Daniel Martinez <danimartinezmarquez@gmail.com>
2025-05-06 20:56:14 +0200

    Save method

--- src/persistence/mysqlauthorrepository.ts
+++ src/persistence/mysqlauthorrepository.ts
@@ -2,33 +2,53 @@ import { RowDataPacket } from "mysql2";
 import { AuthorRepository } from "../application/repositories/authorrespository";
 import { Author } from "../domain/author";
 import { DatabaseManager } from "./dbmanager";
-import { UUID } from "crypto";
 
 interface AuthorModel extends RowDataPacket {
-  idauthors: UUID;
+  idauthors: `${string}-${string}-${string}-${string}-${string}`;
   name: string;
   email: string;
-  image_url: string;
+  image_url: string | null;
 }
+type QueryField = "id" | "name";
+
+const authorFieldMap: Record<QueryField, string> = {
+  id: "idauthors",
+  name: "name",
+};
 
 export class MySQLAuthorRepository implements AuthorRepository {
   constructor(private dbManager: DatabaseManager) {}
 
   async getAuthorById(id: string): Promise<Author | null> {
-    const query = "SELECT * FROM authors WHERE idauthors = ?";
-    const queryParams = [id];
-    const results = await this.dbManager.query<AuthorModel>(query, queryParams);
+    return this.getAuthorBy("id", id);
+  }
+  async getAuthorByName(name: string): Promise<Author | null> {
+    return this.getAuthorBy("name", name);
+  }
+  async save(author: Author): Promise<void> {
+    const query = `
+      INSERT INTO authors (idauthors, name, email, image_url)
+      VALUES (?, ?, ?, ?)
+    `;
+    await this.dbManager.query(query, [
+      author.getAuthorId(),
+      author.getName(),
+      author.getEmail(),
+      author.getImageURL(),
+    ]);
+  }
+
+  private async getAuthorBy(
+    field: QueryField,
+    value: string
+  ): Promise<Author | null> {
+    const columnName = authorFieldMap[field];
+    const query = `SELECT * FROM authors WHERE ${columnName} = ?`;
+    const results = await this.dbManager.query<AuthorModel>(query, [value]);
 
     if (results.length < 1) return null;
 
     const { idauthors, name, email, image_url } = results[0];
-
     return new Author(name, email, image_url ?? "", idauthors);
-  }
-  async getAuthorByName(name: string): Promise<Author | null> {
-    throw new Error("Method not implemented.");
-  }
-  async save(author: Author): Promise<void> {
-    throw new Error("Method not implemented.");
   }
 }
